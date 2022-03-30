fd9b9cd8ed9ca6600627070afc2a0370b5c9eb14
DeanDodds <deandodds88@gmail.com>
2022-03-30 21:15:51 +0000

    login and signing page now directs to profile page

--- app.py
+++ app.py
@@ -44,9 +44,32 @@ def get_contact():
     return render_template("contact.html")
 
 
-@app.route("/get_login")
-def get_login():
+@app.route("/login", methods=["GET", "POST"])
+def login():
     """ Displays login page """
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for("profile", username=session["user"]))
+
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # if the username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
@@ -70,14 +93,18 @@ def signup():
 
          # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
-        flash("Sign upSuccessfull!")
+        flash("Sign up successfull!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("signup.html")
 
 
-@app.route("/get_profile")
-def get_profile():
-    """ Displays profile page """
-    return render_template("profile.html")
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # get the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
 
 
 if __name__ == "__main__":
