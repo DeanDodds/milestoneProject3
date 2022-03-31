1f7e932d040ea01fbf48800d458d2da32bc5e8e8
DeanDodds <deandodds88@gmail.com>
2022-03-31 18:29:14 +0000

    add ingredents to list function added to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,26 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('select').formSelect();
 });
+
+// Collapsibles
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+
+// Get ingrendents lists 
+let addIngredientsBtn = document.getElementById("add-item-button")
+addIngredientsBtn.addEventListener("click", addIngredients)
+
+function addIngredients() {
+
+    let ingredientsList = document.getElementById('ingredients-list');
+    let ingredients = document.getElementById('ingredients').value;
+    let amount = document.getElementById('amount').value;
+    let units = document.getElementById('units').value;
+    let ingredientsListItem = `${amount + units} ${ingredients}`;
+    let li = document.createElement('li');
+    let node = document.createTextNode(ingredientsListItem);
+
+    li.appendChild(node);
+    ingredientsList.appendChild(li);
+}
