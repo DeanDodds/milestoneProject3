8ef1c4df749f5cc9b3691969765f0417360e0dd2
DeanDodds <deandodds88@gmail.com>
2022-03-31 14:10:36 +0000

    log out functionality added

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def signup():
     if request.method == "POST":
         # checks if user already exists
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()}) 
+            {"username": request.form.get("username").lower()})
 
         if existing_user:
             flash("Username already exists")
@@ -100,11 +100,22 @@ def signup():
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    """"displays profile page"""""
     # get the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+        
+    if session["user"]:
+        return render_template("profile.html", username=username)
 
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    session.pop("user")
+    flash("You have been logged out")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
