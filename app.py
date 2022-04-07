df7bfe142c8dfc17b9e6f619e7967b058edf8fc9
DeanDodds <deandodds88@gmail.com>
2022-04-07 17:39:36 +0000

    comments added to help with readabliity of code

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def profile(username):
         {"username": session["user"]})["username"]
     # gets recipes from database
     recipes = mongo.db.recipes.find()
-
+    # checks if user is in session 
     if session["user"]:
         return render_template(
             "profile.html", username=username, recipes=recipes)
@@ -129,6 +129,7 @@ def profile(username):
 @app.route("/logout")
 def logout():
     """ remove user from session cookie """
+    # Removes session user 
     session.pop("user")
     flash("You have been logged out")
     return redirect(url_for("login"))
@@ -139,6 +140,7 @@ def addrecipe():
     """"get recipe catergoies and cusine
     from database and displays addrecipe page"""
     if request.method == "POST":
+        # Get the data from the add recipe form
         recipe = {
             "author": session["user"],
             "catergory_name": request.form.get("catergory_name"),
@@ -148,15 +150,17 @@ def addrecipe():
             "instructions": request.form.get("instructions"),
             "img_url": request.form.get("img_url"),
             "description": request.form.get("description"),
-            "rating": [],
-            "star_ratings": []
+            "star_ratings": = [],
+            "rating" = 0
         }
 
+        # Inserts data into the recipe database
         mongo.db.recipes.insert_one(recipe)
         flash("Task Successfully Added")
         return redirect(url_for("profile", username=session["user"]))
-
+    # Gets the cuisine names from database
     cuisine = mongo.db.cuisine.find().sort("cuisine_name, 1")
+    # Gets the catergorie names from database
     catergories = mongo.db.catergories.find().sort("catergory_name, 1")
     return render_template(
         "addrecipe.html", cuisine=cuisine, catergories=catergories)
@@ -165,6 +169,7 @@ def addrecipe():
 @app.route("/editrecipe/<recipe_id>", methods=["GET", "POST"])
 def editrecipe(recipe_id):
     if request.method == "POST":
+        # Get the data feom the add recipe form
         submit = {
             "author": session["user"],
             "catergory_name": request.form.get("catergory_name"),
@@ -174,13 +179,17 @@ def editrecipe(recipe_id):
             "instructions": request.form.get("instructions"),
             "img_url": request.form.get("img_url"),
             "description": request.form.get("description")
+            "star_ratings": = [],
+            "rating" = 0
         }
         mongo.db.recipes.replace_one(
                     {"_id": ObjectId(recipe_id)}, submit)
         flash("Recipe Successfully Updated")
-
+    # Find the recipe in the database 
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    # Gets the catergouries from the database
     catergories = mongo.db.catergories.find().sort("catergory_name, 1")
+    # Gets the cuisine names from the database
     cuisine = mongo.db.cuisine.find().sort("cuisine_name, 1")
     return render_template(
         "editrecipe.html", 
@@ -219,28 +228,30 @@ def add_recipe_rating(recipe_id):
 
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
-    """Delete recipe from databse"""
-    mongo.db.recipes.delete_one()
+    """Delete recipe from database"""
+    # Deletes the document from the database 
+    mongo.db.recipes.delete_one({"_id": ObjectId(recipe_id)})
     flash("Recipe Successfully Deleted")
     return redirect(url_for("get_recipes"))
 
 
 @app.route("/add_favourites/<recipe_id>")
 def add_favourites(recipe_id):
-
+    # Gets the user form database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
+    # Gets the user id form database
     user_id = mongo.db.users.find_one(
         {"username": session["user"]})["_id"]
-
+    # adds the recipe id to the favourites array
     mongo.db.users.update_one(
         {"_id": ObjectId(user_id)}, 
         {"$addToSet": {"favourites": ObjectId(recipe_id)}})
+    # User comfirmation message
     flash("Recipe saved to favourites!")
-
     # gets recipes from database
     recipes = mongo.db.recipes.find()
-
+    #if user signed in 
     if session["user"]:
         return render_template(
             "profile.html", username=username, recipes=recipes,
@@ -249,14 +260,18 @@ def add_favourites(recipe_id):
 
 @app.route("/favourites/)")
 def favourites():
+    """"Displays the users saved recipes on the favourites page"""
+    # Gets the user form database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
+    # Gets the user id form database
     user_id = mongo.db.users.find_one(
         {"username": session["user"]})["_id"]
+    # Gets Favourites ffrom the database 
     favourites = mongo.db.users.find_one(
         {"username": session["user"]})["favourites"]
+    # Gets the recipes from database
     recipes = mongo.db.recipes.find()
-    
     return render_template(
         "favourites.html", user_id=user_id,
         username=username,  favourites=favourites, recipes=recipes)
@@ -264,18 +279,24 @@ def favourites():
 
 @app.route("/remove_from_favourites/<recipe_id>")
 def remove_from_favourites(recipe_id):
+    """removes saved recipes from favourites page"""
+    # Gets the user form database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
+    # Gets the user id form database
     user_id = mongo.db.users.find_one(
         {"username": session["user"]})["_id"]
-    favourites = mongo.db.users.find_one(
-        {"username": session["user"]})["favourites"]
-    recipes = mongo.db.recipes.find()
-    flash("recipe removed from favourites")
-
+    # Removes recipe id from the favourites array
     mongo.db.users.update_one(
         {"_id": ObjectId(user_id)}, 
         {"$pull": {"favourites": ObjectId(recipe_id)}})
+    # gets users new favorites from the database 
+    favourites = mongo.db.users.find_one(
+        {"username": session["user"]})["favourites"]
+    # Gets recipes from the database  
+    recipes = mongo.db.recipes.find()
+    flash("recipe removed from favourites")
+
     return render_template(
         "favourites.html", user_id=user_id,
         username=username, favourites=favourites, recipes=recipes)
@@ -283,6 +304,8 @@ def remove_from_favourites(recipe_id):
 
 @app.route("/recipe_page/<recipe_id>")
 def recipe_page(recipe_id):
+    """Display indivdaul recipe page """
+    # Gets the recipe from database with the recipe id  
     recipe = mongo.db.recipes.find_one(
         {"_id": ObjectId(recipe_id)})
     return render_template(
