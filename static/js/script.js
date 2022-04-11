424ada0541f0246bd4779319e1a2823fd1010293
DeanDodds <deandodds88@gmail.com>
2022-04-11 21:14:33 +0000

    unused code removed

--- static/js/script.js
+++ static/js/script.js
@@ -35,24 +35,6 @@ $('.fixed-action-btn').floatingActionButton({
     toolbarEnabled: true
 });
 
-// Get ingrendents lists 
-let addIngredientsBtn = document.getElementById("add-item-button")
-addIngredientsBtn.addEventListener("click", addIngredients)
-
-function addIngredients() {
-
-    let ingredientsList = document.getElementById('ingredients-list');
-    let ingredients = document.getElementById('ingredients').value;
-    let amount = document.getElementById('amount').value;
-    let units = document.getElementById('units').value;
-    let ingredientsListItem = `${amount + units} ${ingredients}`;
-    let li = document.createElement('li');
-    let node = document.createTextNode(ingredientsListItem);
-
-    li.appendChild(node);
-    ingredientsList.appendChild(li);
-}
-
 // star rating imput code from - https://www.codingnepalweb.com/star-rating-html-css-javascript/
 const btn = document.querySelector("button");
 const post = document.querySelector(".post");
